76a662e35f6aaaf27305588952e8933b8a319682
Greg Degruy <gregory.degruy@gmail.com>
2016-09-20 13:54:27 -0700

    Adding pokemon number lookup dialog

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -23,6 +23,28 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Welcome to Pokedex bot! Give me a Pokemon number.');
+    },
+    function (session, results) {
+        session.send('Looking up Pokemon number %s ', results.response);
+    },
+    function (session) {
+
+    }
+]);
+
+function loopupPokemon(number) {
+    var callURL = "http://pokeapi.co/api/v2/pokemon" + number + "/";
+
+    xhr.open("GET", callURL, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var serverResponse = xhr.responseText;
+        }
+    };
+    xhr.send(null);
+
+    return serverResponse;
+}
