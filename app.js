686e31305167c9a0190cdf4a9e13c0863dbf0f03
Greg Degruy <gregory.degruy@gmail.com>
2016-09-21 00:46:24 -0700

    Getting JSON response

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var restify = require("restify");
 var builder = require("botbuilder");
-var pokeapi = "http://pokeapi.co/api/v2/"
-var pkmnOfficialArtwork = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/";
+var http = require("http");
+var request = require("request");
+var restify = require("restify");
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
+var pokeapi = "http://pokeapi.co/api/v2/"
+var pkmnOfficialArtwork = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/";
+
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+    console.log("%s listening to %s", server.name, server.url);
 });
 
 // Create chat bot
@@ -19,21 +23,25 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+server.post("/api/messages", connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', [
+bot.dialog("/", [
     function (session) {
         session.beginDialog("/welcome");
     },
     function (session, results) {
-        session.send("Looking up Pokemon number %s ...", results.response);
+        session.send("Let\'s see what I can find on this pokemon . . .");
+        var name = lookupPkmn(121);
+        session.send("Oh, it\s %s !", name);
         var card = new builder.HeroCard(session)
+            .title(name)
+            .text("Type")
             .images([
-                 builder.CardImage.create(session, pkmnOfficialArtwork + results.response + ".png")
+                builder.CardImage.create(session, pkmnOfficialArtwork + results.response + ".png")
             ]);
         var msg = new builder.Message(session).attachments([card]);
         session.send(msg);
@@ -49,7 +57,7 @@ bot.dialog("/welcome", [
             .title("Pokedex Bot")
             .text("Explore the Pokemon World")
             .images([
-                 builder.CardImage.create(session, pkmnOfficialArtwork + "351.png")
+                builder.CardImage.create(session, "http://cdn.bulbagarden.net/upload/3/30/FireRed_LeafGreen_Professor_Oak.png")
             ]);
         var msg = new builder.Message(session).attachments([card]);
         session.send(msg);
@@ -59,16 +67,27 @@ bot.dialog("/welcome", [
 ]);
 
 function lookupPkmn(number) {
-    var callURL = pokeapi + "pokemon" + number + "/";
+    var reqURL = pokeapi + "pokemon/" + number + "/";
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", callURL, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            var serverResponse = xhr.responseText;
+    var headers = {
+        "User-Agent": "Greg Universe/0.2",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    var options = {
+        url: reqURL,
+        method: "GET",
+        headers: headers
+    }
+
+    var x;
+
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            x = JSON.parse(body);
+            console.log(x);
         }
-    };
-    xhr.send(null);
+    });
 
-    return serverResponse;
+    return x.forms[0].name;
 }
